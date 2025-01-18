e3e5fe3cbfe84588d578804cd4faeffc0e284879
sexysteve0 <39505545+sexysteve0@users.noreply.github.com>
2025-01-18 14:45:38 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,20 @@
+import boto3
 import json
+import os
 
 def lambda_handler(event, context):
-    """
-    A basic AWS Lambda function for testing AWS CodePipeline integration.
-    """
-    print("AWS Lambda function invoked!")
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Hello, AWS CodePipeline and GitHub integration works Stever!",
+    try:
+        message = {
+            "status": "success",
+            "message": "Hello from Lambda!",
             "input": event
-        })
-    }
+        }
+        return {
+            "statusCode": 200,
+            "body": json.dumps(message)
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
